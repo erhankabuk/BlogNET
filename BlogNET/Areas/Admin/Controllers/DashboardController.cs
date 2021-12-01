4e408a6cc562d71ac3a94f81fadf3ccebad877ce
erhankabuk <erhankabuk@hotmail.com>
2021-12-01 11:01:39 +0300

    Dashboard

--- BlogNET/Areas/Admin/Controllers/DashboardController.cs
+++ BlogNET/Areas/Admin/Controllers/DashboardController.cs
@@ -1,18 +1,31 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿
+using BlogNET.Data;
+using BlogNET.Areas.Admin;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using BlogNET.Areas.Admin.Models;
+using BlogNET.Areas.Admin.Controllers;
 
 namespace BlogNET.Areas.Admin.Controllers
 {
    
     public class DashboardController : AdminBaseController
     {
+        private readonly ApplicationDbContext _db;
+
+        public DashboardController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var vm = new DashboardViewModel()
+            {
+                CategoryCount = _db.Categories.Count(),
+                PostCount = _db.Posts.Count(),
+                UserCount = _db.Users.Count()
+            };
+            return View(vm);
         }
     }
 }
---
+++ BlogNET/Areas/Admin/Models/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogNET.Areas.Admin.Models
+{
+    public class DashboardViewModel
+    {
+        public int CategoryCount { get; set; }
+        public int PostCount { get; set; }
+        public int UserCount { get; set; }
+        //public int CommentCount { get; set; }
+    }
+}
